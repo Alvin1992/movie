90537b3329f7e7b8667ad82e7fc1bfcbb5eff6ab
Alvin1992 <fanrx_1992@163.com>
2016-11-11 09:58:23 -0800

    test

--- app.js
+++ app.js
@@ -2,4 +2,11 @@
  * Created by Administrator on 2016/10/26.
  */
 
+var express = require('express');
+var app = express();
 
+app.get('/test', function (req, res, next) {
+    res.send('Hello World');
+});
+
+app.listen(8080);
